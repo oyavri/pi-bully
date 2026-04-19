189bc2ed21d5e98c03b57da1726f51a98427c04b
oyavri <oyavri@gmail.com>
2026-04-19 17:36:38 +0300

    Add new configurations for election

--- config/election.go
+++ config/election.go
@@ -3,11 +3,15 @@ package config
 import "time"
 
 type ElectionConfig struct {
-	ElectionTimeout time.Duration
+	ElectionTimeout       time.Duration
+	AnnounceRetryInterval time.Duration
+	SendRetryInterval     time.Duration
 }
 
 func loadElectionConfig() ElectionConfig {
 	return ElectionConfig{
-		ElectionTimeout: getDurationEnv("ELECTION_TIMEOUT", 5*time.Second),
+		ElectionTimeout:       getDurationEnv("ELECTION_TIMEOUT", 5*time.Second),
+		AnnounceRetryInterval: getDurationEnv("ELECTION_ANNOUNCE_RETRY_INTERVAL", 200*time.Millisecond),
+		SendRetryInterval:     getDurationEnv("ELECTION_SEND_RETRY_INTERVAL", 100*time.Millisecond),
 	}
 }
